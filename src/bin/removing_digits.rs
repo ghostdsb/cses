623d358252a7d999ee82c5a88175da13533624d9
Dibyanshu <dibyanshu.bhoi@gmail.com>
2024-01-24 00:06:54 +0530

    removing digits

---
+++ src/bin/removing_digits.rs
@@ -0,0 +1,27 @@
+use cses::util::input_single;
+
+fn main() {
+    let input: u64 = input_single(0u64);
+    let ans = solve(input);
+    println!("{ans}");
+}
+
+fn solve(x: u64) -> u64 {
+    // let mut memo: Vec<u64> = vec![0; x as usize +1];
+    let mut x = x;
+    let mut count = 0;
+    while x != 0{
+        let m = max_digit(x) as u64;
+        x -= m;
+        // println!("{x} {m}");
+        count += 1;
+    }
+    count
+}
+
+fn max_digit(x: u64) -> u8{
+    let mut y = x.to_string().chars().map(|d| d as u8 - '0' as u8).collect::<Vec<u8>>();
+    y.sort();
+    // dbg!(&y);
+    *y.last().unwrap()
+}
--- src/bin/solution_file.rs
+++ src/bin/solution_file.rs
@@ -1,28 +1,28 @@
 use std::fmt::Debug;
 use std::{io, str::FromStr};
 
-const MAX: u64 = 10_u64.pow(9) + 7;
 fn main() {
-    let input:Vec<u64> = input_vector(vec![]);
-    let (_n,x) = (input[0], input[1]);
-    let coins:Vec<u64> = input_vector(vec![]);
-    let ans = solve(x, &coins);
+    let input: u64 = input_single(0u64);
+    let ans = solve(input);
     println!("{ans}");
 }
 
-fn solve(x: u64, coins: &Vec<u64>) -> u64 {
-    let mut memo: Vec<u64> = vec![0; x as usize +1];
-    memo[0] = 1;
-    for i in 1..x+1{
-        for coin in coins.iter(){
-            if i >= *coin{
-                memo[i as usize] = memo[i as usize] % MAX + memo[(i-coin) as usize] % MAX;
-            }
-        }
+fn solve(x: u64) -> u64 {
+    let mut x = x;
+    let mut count = 0;
+    while x != 0{
+        let m = max_digit(x) as u64;
+        x -= m;
+        count += 1;
     }
-    *memo.last().unwrap() % MAX
+    count
 }
 
+fn max_digit(x: u64) -> u8{
+    let mut y = x.to_string().chars().map(|d| d as u8 - '0' as u8).collect::<Vec<u8>>();
+    y.sort();
+    *y.last().unwrap()
+}
 
 
 fn input_single<T>(default: T) -> T
