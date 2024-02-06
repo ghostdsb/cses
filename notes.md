19b3e45ea0a5e740f9880255db2eeb38b5b7bd8d
ghostdsb <dibyanshu.bhoi@gmail.com>
2024-02-06 23:31:50 +0530

    feat: highscore

---
+++ notes.md
@@ -0,0 +1,4 @@
+SINGLE SOURCE SHORTEST/LONGEST PATH
+- BFS when weight is 1 (queue)
+- Dijkstra when weight > 1 (priority queue; min/max heap)
+- Bellman Ford when negative weights present (array of nodes)(DP)
--- src/bin/graph_highscore.rs
+++ src/bin/graph_highscore.rs
@@ -1,5 +1,6 @@
-use std::fmt::Debug;
-use std::{io, str::FromStr};
+//brute forced as constraints low
+
+use cses::util::{input_single, input_vector};
 
 #[derive(Debug, Clone, Copy)]
 struct Edge{
@@ -54,33 +55,5 @@ fn main() {
         println!("-1");
     }else{
         println!("{}", -distance[n]);
-    }
-}
-
-fn input_single<T>(default: T) -> T
-where
-    T: FromStr + Debug,
-    <T as FromStr>::Err: Debug,
-{
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => input.trim().parse::<T>().unwrap(),
-        Err(_) => default,
-    }
-}
-
-fn input_vector<T>(default: Vec<T>) -> Vec<T>
-where
-    T: FromStr + Debug,
-    <T as FromStr>::Err: Debug,
-{
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => input
-            .as_str()
-            .split_whitespace()
-            .map(|n| n.parse().unwrap())
-            .collect(),
-        Err(_) => default,
     }
 }
