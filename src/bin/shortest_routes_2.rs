a231462eb2e9317496948ab64482ef0f9364d043
ghostdsb <dibyanshu.bhoi@gmail.com>
2024-02-06 13:14:21 +0530

    feat: shortest_routes II

--- src/bin/shortest_routes_2.rs
+++ src/bin/shortest_routes_2.rs
@@ -1,5 +1,6 @@
-use std::fmt::Debug;
-use std::{io, str::FromStr};
+//brute forced as constraints low
+
+use cses::util::{input_single, input_vector};
 
 fn main() {
     let input: Vec<usize> = input_vector(vec![]);
@@ -32,33 +33,5 @@ fn main() {
         } else {
             println!("-1");
         }
-    }
-}
-
-fn input_single<T>(default: T) -> T
-where
-    T: FromStr + Debug,
-    <T as FromStr>::Err: Debug,
-{
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => input.trim().parse::<T>().unwrap(),
-        Err(_) => default,
-    }
-}
-
-fn input_vector<T>(default: Vec<T>) -> Vec<T>
-where
-    T: FromStr + Debug,
-    <T as FromStr>::Err: Debug,
-{
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => input
-            .as_str()
-            .split_whitespace()
-            .map(|n| n.parse().unwrap())
-            .collect(),
-        Err(_) => default,
     }
 }
